6b72a818dad07558d9817e216b2982f4748ee2ac
nvelasquez <zerofull800@hotmail.com>
2019-10-20 16:28:59 -0400

    First commit

---
+++ XamarinEstilosMod2/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinEstilosMod2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private bool originalState = true;
+        public MainPage()
+        {
+            InitializeComponent();
+            Resources["buttonTheme"] = Application.Current.Resources["Light"];
+        }
+
+        public void cambiarTema(object sender, EventArgs events)
+        {
+            if (originalState)
+            {
+                Resources["buttonTheme"] = Application.Current.Resources["Dark"];
+                originalState = false;
+            }
+            else
+            {
+                Resources["buttonTheme"] = Application.Current.Resources["Light"];
+                originalState = true;
+            }
+        }
+    }
+}
---
+++ XamarinEstilosMod2/Theme/Bootstrap.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinEstilosMod2.Theme
+{
+    public partial class Bootstrap : ResourceDictionary
+    {
+        public Bootstrap()
+        {
+            InitializeComponent();
+        }
+    }
+}
